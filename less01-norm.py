a4b5121d5f5a4843ae80ec32b8c2d8ca04398aeb
Jecteroid <tema.stafeev@yandex.ru>
2018-07-08 22:06:44 +0300

    Сделал с помощью 2-ух переменных.(Сразу не заметил)

--- less01-norm.py
+++ less01-norm.py
@@ -1,11 +1,5 @@
 # coding utf-8
 
-def substitution(var1, var2):
-    var3 = var1
-    var1 = var2
-    var2 = var3
-    return var1, var2
-
 # Задача 1
 numb = int(input('Введите число в диапазоне (0, 10): '))
 while numb >= 10 or numb <= 0:
@@ -19,5 +13,6 @@ else:
 var_1 = input('Введите что-нибудь: ')
 var_2 = input('Введите еще чего-нибудь: ')
 
-var_1, var_2 = substitution(var_1, var_2)
+var_1, var_2 = var_2, var_1
 print('После замены значений:', var_1, var_2)
+
